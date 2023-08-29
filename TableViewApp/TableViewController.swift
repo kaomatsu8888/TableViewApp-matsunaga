e59dd4f8687980403d3c73b86ae8fabdadd4450f
Kaoru Matsunaga <m.kaoru0830@gmail.com>
2023-08-29 16:24:19 +0900

    kannsei

--- TableViewApp/TableViewController.swift
+++ TableViewApp/TableViewController.swift
@@ -9,27 +9,36 @@ import UIKit
 
 class TableViewController: UITableViewController {
     // 画面内に表示する名前を入れています。
-    var names: [String] = [
-        "阿部",
-        "石田",
-        "上田",
-        "遠藤",
-        "及川",
-        "梶原",
-        "菊池",
-        "工藤",
-    ]
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    var taskArray: [String] = []
+    //      "阿部",
+    //        "石田",
+    //        "上田",
+    //        "遠藤",
+    //        "及川",
+    //        "梶原",
+    //        "菊池",
+    //        "工藤",
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let userDefaults = UserDefaults.standard
+        
+        //"add"というキーで保存された値がなにかある -> 値をtaskArrayへ
+        if userDefaults.object(forKey: "add") != nil {
+            taskArray = userDefaults.object(forKey: "add") as! [String]
+            print(taskArray)
+        }
+        //tableViewを更新
+        tableView.reloadData()
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     // MARK: - Table view data source
     // セクションの数を返すメソッド
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -39,63 +48,76 @@ class TableViewController: UITableViewController {
     // セクションの中の行(セル)の数
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        // 「配列の変数名.count」で配列の要素数を取得できる
-        return names.count
+        // [var of arrangement.count] means getting number of elements arrangement
+        return taskArray.count
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //セルのオブジェクトを作成します
         let cell = tableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath)
         //
-
-        cell.textLabel?.text = names[indexPath.row]
+        
+        cell.textLabel?.text = taskArray[indexPath.row]
         return cell
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            taskArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            print(taskArray)
+            
+            // Save the updated taskArray to UserDefaults
+            let userDefaults = UserDefaults.standard
+            userDefaults.set(taskArray, forKey: "add")
+            userDefaults.synchronize()
+            print(taskArray)
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
